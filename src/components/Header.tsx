197179f03ff0124e88d3e0461ed5d4a27623edf3
Lee Mander <lee.mander@gmail.com>
2023-11-24 13:56:55 +0000

    Adjust styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,11 @@ export default function Header() {
   return (
     <header className=" p-4 bg-blue-300">
       <div className="container mx-auto m-4 flex justify-between">
-        <h1>Paws 'n' Claws</h1>
+        <h1 className="text-3xl font-serif text-blue-900 font-semibold">
+          Paws 'n' Claws
+        </h1>
         <nav>
-          <ul className="flex gap-4">
+          <ul className="flex gap-4 text-lg">
             <li>
               <Link href="/">Home</Link>
             </li>
